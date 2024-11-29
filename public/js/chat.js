8f045395a39f7368e5c5dfba52754c5fac093e77
chison2707 <chiphansonzz17@gmail.com>
2024-11-29 16:00:31 +0700

    làm tính năng chat cơ bản

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-var socket = io();
+// CLIENT_SEND_MESSAGE
+const formsendData = document.querySelector(".chat .inner-form");
+if (formsendData) {
+    formsendData.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const content = e.target.elements.content.value;
+        if (content) {
+            socket.emit("CLIENT_SEND_MESSAGE", content);
+            e.target.elements.content.value = "";
+        }
+    });
+}
+//END CLIENT_SEND_MESSAGE
