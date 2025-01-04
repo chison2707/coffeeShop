ee5652e749e23d8dda68135ae5c52992249b025a
chison2707 <chiphansonzz17@gmail.com>
2025-01-04 11:10:23 +0700

    fix dashboard

--- middlewares/admin/uploadCloud.middleware.js
+++ middlewares/admin/uploadCloud.middleware.js
@@ -3,7 +3,6 @@ const uploadToCloudinary = require("../../helpers/uploadToCloudinary");
 module.exports.upload = async (req, res, next) => {
     if (req.file) {
         const result = await uploadToCloudinary(req.file.buffer);
-        console.log(result);
         req.body[req.file.fieldname] = result;
     }
     next();
